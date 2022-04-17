20841008dd140597e97522b94b1900952e2f060c
Rubén García Pedrejón <62309228+rubengp39@users.noreply.github.com>
2022-04-17 23:56:34 +0200

    aboutme

--- js/app.js
+++ js/app.js
@@ -9,3 +9,16 @@ function stickyNavbar() {
 stickyNavbar();
 
 window.addEventListener("scroll", stickyNavbar);
+
+// Scroll Reveal
+
+let sr = ScrollReveal({
+    duration: 2000,
+    distance: "90px",
+});
+
+sr.reveal(".showcase-info", { delay: 300 });
+sr.reveal(".showcase-image", {
+    origin: "top",
+    delay: 300
+});
